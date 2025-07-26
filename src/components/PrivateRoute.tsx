871fa96433a7b0ef3e5e8115a9b2a11eb6c574a2
tharan-26 <tharanyaa0206@gmail.com>
2025-07-26 13:36:02 +0530

    Fix: Update PrivateRoute to use Firebase auth state with react-firebase-hooks

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { auth } from "../firebase/firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const PrivateRoute: React.FC = () => {
-  const isLoggedIn = !!auth.currentUser;
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <p>Loading...</p>;
+
+  return user ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
